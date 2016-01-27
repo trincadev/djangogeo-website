1861ddd153b464bd2e07ebcd9c008c8f79f0c5b6
aletrn <alenewmail-dev@yahoo.it>
2016-01-27 21:46:01 +0100

    initial commit (openshift integration)

--- setup.py
+++ setup.py
@@ -1,10 +1,12 @@
 from setuptools import setup
 
-setup(name='YourAppName',
+setup(name='djangogeo',
       version='1.0',
-      description='OpenShift App',
-      author='Your Name',
-      author_email='example@example.com',
+      description='OpenShift DjangoGeo',
+      author='Ale',
+      author_email='dev@trincatornidor.com',
       url='http://www.python.org/sigs/distutils-sig/',
-#      install_requires=['Django>=1.3'],
-     )
+      install_requires=['Django>=1.8,<1.9'],
+      dependency_links=['https://pypi.python.org/simple/django'],
+)
+
